43c84916856f02a32253719de20d1efd7eadedbc
SamilSanchez <sanchezsamil@gmail.com>
2020-05-07 15:23:15 -0400

    learn generate guards to control access to view

--- src/app/intro/intro.page.ts
+++ src/app/intro/intro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-intro',
@@ -35,7 +37,12 @@ export class IntroPage implements OnInit {
     }
   ]
 
-  constructor() { }
+  constructor(private router:Router, private storage: Storage) { }
+
+  finish() {
+    this.storage.set('isIntroShowed', true);
+    this.router.navigateByUrl("/home");
+  }
 
   ngOnInit() {
   }
