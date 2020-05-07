b444aa37dce03cc12b3bdb3ac5d6286fc4302034
SamilSanchez <sanchezsamil@gmail.com>
2020-05-07 12:02:58 -0400

    add new page intro

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 1,
+    centeredSlides: true,
+    speed: 400
+  }
+
+  slides = [{
+    'title': 'Escucha tu música',
+    'subTitle': 'EN CUALQUIER LUGAR',
+    'description': ' Los mejores álbumes, las mejores canciones. Escucha y comparte en cualquier \
+    momento, a todas horas.',
+    'icon': 'play'
+    },{
+    'title': 'Disfruta de nuetro reproductor',
+    'subTitle': 'DE VIDEOS INCREIBLES',
+    'description': ' Entra al modo video de nuestro reproductor y obten acceso a clips, \
+     documentales y making offs increibles de tu artista favorito.',
+    'icon': 'bicycle'
+    },{
+    'title': 'Accede al exclusivo',
+    'subTitle': 'MODO DEPORTE',
+    'description': 'Crea una playlist basada en tu actividad física. <br/> \
+    Ten reportes y acceso a lo que necesites, integrado con GPS!',
+    'icon': 'videocam'
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
